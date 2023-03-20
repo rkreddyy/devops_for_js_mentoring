64b3d773a4efc984608007aa8be4893cfa3216df
RaviKiran Anumula <Ravikiran_Anumula@epuspriw04ed.ht.home>
2023-03-19 23:20:12 -0400

    updated the logic

--- module_4/lab_1/quality-check.sh
+++ module_4/lab_1/quality-check.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname "$(readlink -f "$BASH_SOURCE")")
-ROOT_DIR=${SCRIPT_DIR%/*}
-CLIENT_BUILD_DIR=$ROOT_DIR/dist/static
-
-cd $ROOT_DIR/client && npm run lint && npm run test && npm audit
+# Move to project folder.
+cd ../../shop-angular-cloudfront && npm run lint && npm run test && npm audit
 
 if [ $? -eq 0 ]; then
     echo "SUCCESS"
--- module_4/lab_1/update-pipeline-definition.sh
+++ module_4/lab_1/update-pipeline-definition.sh
@@ -1,60 +1,95 @@
-#!/bin/bash
+#validating if jq is installed
 
-defaultPipeline="pipeline"
-customPipelineChoice="custom"
+if ! [[ -f $(which jq) ]]; then
+  echo "In order to run this script properly, you need to have the jq lib installed."
+  echo "If you are a Linux user, to install jq type in a terminal: sudo apt-get install jq and press enter"
+  echo "If you are a macOS user, to install jq type in a terminal: brew install jq and press enter(homebrew is required for this)."
+  echo "Finally, if you are a windows user, you can download the executables here: stedolan.github.io/jq/download or if you have chocolatey just open a terminal with administrator privileges and type: chocolatey install jq."
+  exit 0
+fi
 
-# colors
-red=$'\e[1;31m'
-grn=$'\e[1;32m'
-end=$'\e[0m'
+#validating if first argument is the path of the JSON file
 
-checkJQ() {
-  # jq test
-  type jq >/dev/null 2>&1
-  exitCode=$?
+if ! [[ "$1" =~ .*.json ]]; then
+  echo "Error! The first argument should be the path of the JSON file you want to change. It is supposed to be in this folder's parent directory." 1>&2
+  exit 64
+fi
 
-  if [ "$exitCode" -ne 0 ]; then
-    printf "  ${red}'jq' not found! (json parser)\n${end}"
-    printf "    MacOS Installation:  https://jira.amway.com:8444/display/CLOUD/Configure+PowerShell+for+AWS+Automation#ConfigurePowerShellforAWSAutomation-MacOSSetupforBashScript\n"
-    printf "    Ubuntu Installation: sudo apt install jq\n"
-    printf "    Redhat Installation: sudo yum install jq\n"
-    jqDependency=0
-  else
-    if [ "$DEBUG" -eq 1 ]; then
-      printf "  ${grn}'jq' found!\n${end}"
-    fi
-  fi
+#validating if there are main properties on the file
 
-  if [ "$jqDependency" -eq 0 ]; then
-    printf "${red}Missing 'jq' dependency, exiting.\n${end}"
-    exit 1
+validatingJson() {
+  key=$(grep "$1" ./pipeline.json)
+  if [[ "$key" = "" ]]; then
+    echo "Error! The JSON file is not in the correct structure." 1>&2
+    exit 64
+
   fi
 }
 
-# perform checks:
-checkJQ
+validatingJson 'pipeline'
+validatingJson 'stages'
+validatingJson 'actions'
+validatingJson 'Branch'
+validatingJson 'Owner'
+validatingJson 'PollForSourceChanges'
 
-echo -n "Enter a CodePipeline name (or $customPipelineChoice)(default: $defaultPipeline): "
-read -r pipelineName
+#script to create the updated JSON file
 
-pipelineName=$(checkPipelineName "$pipelineName")
+cp $1 "pipeline-$(date +'%d-%m-%Y_%Hh%Mm%Ss').json"
+cd ..
+file=$(find . -type f -iname "pipeline-$(date +'%d-%m-%Y_%Hh%Mm%Ss').json" | tail -1)
 
-if [ "$pipelineName" = "$customPipelineChoice" ]; then
-  echo -n "Enter a CodePipeline name: "
-  read -r pipelineName
+overwrittingFile() {
+  cp temp $file
+  rm temp
+}
+
+jq 'del(.metadata)' $file >temp
+overwrittingFile
+newVersion=$(jq '.pipeline.version+1' $file)
+jq --arg newVersion "$newVersion" '.pipeline.version = $newVersion' $file >temp
+overwrittingFile
+
+branchFlag="main"
+ownerFlag="Thirdparty"
+pollForSourceChangesFlag=false
+configurationFlag="[{\"name\":\"BUILD_CONFIGURATION\",\"value\":\"none\",\"type\":\"PLAINTEXT\"}]"
+
+while [ "${1:-}" != "" ]; do
+  case $1 in
+  "--branch")
+    shift
+    branchFlag=$1
+    ;;
+  "--owner")
+    shift
+    ownerFlag=$1
+    ;;
+  "--poll-for-source-changes")
+    shift
+    pollForSourceChangesFlag=$1
+    ;;
+  "--configuration")
+    shift
+    configurationFlag="[{\"name\":\"BUILD_CONFIGURATION\",\"value\":\"$1\",\"type\":\"PLAINTEXT\"}]"
+    ;;
+  esac
+  shift
+done
+
+if ! [[ $branchFlag = "main" && $ownerFlag = "Thirdparty" && $pollForSourceChangesFlag = false && $configurationFlag = "[{\"name\":\"BUILD_CONFIGURATION\",\"value\":\"none\",\"type\":\"PLAINTEXT\"}]" ]]; then
+  jq --arg branchFlag "$branchFlag" '.pipeline.stages[0].actions[0].configuration.Branch = $branchFlag' $file >temp
+  overwrittingFile
+  jq --arg ownerFlag "$ownerFlag" '.pipeline.stages[0].actions[0].configuration.Owner = $ownerFlag' $file >temp
+  overwrittingFile
+  jq --arg pollForSourceChangesFlag "$pollForSourceChangesFlag" '.pipeline.stages[0].actions[0].configuration.PollForSourceChanges = $pollForSourceChangesFlag' $file >temp
+  overwrittingFile
+  jq --arg configurationFlag "$configurationFlag" '.pipeline.stages[0].actions[0].configuration.EnvironmentVariables = $configurationFlag' $file >temp
+  overwrittingFile
+  jq --arg configurationFlag "$configurationFlag" '.pipeline.stages[1].actions[0].configuration.EnvironmentVariables = $configurationFlag' $file >temp
+  overwrittingFile
+  jq --arg configurationFlag "$configurationFlag" '.pipeline.stages[2].actions[0].configuration.EnvironmentVariables = $configurationFlag' $file >temp
+  overwrittingFile
+  jq --arg configurationFlag "$configurationFlag" '.pipeline.stages[3].actions[0].configuration.EnvironmentVariables = $configurationFlag' $file >temp
+  overwrittingFile
 fi
-
-defaultBranchName="develop"
-
-echo -n "Enter a source branch to use (default: $defaultBranchName): "
-read -r branchName
-branchName=${branchName:-$defaultBranchName}
-
-pipelineJson="pipeline.json"
-
-# upd source branch
-jq --arg branchName "$branchName" '.pipeline.stages[0].actions[0].configuration.BranchName = $branchName' "$pipelineJson" >tmp.$$.json && mv tmp.$$.json "$pipelineJson"
-# remove metadata
-jq 'del(.metadata)' "$pipelineJson" >tmp.$$.json && mv tmp.$$.json "$pipelineJson"
-
-exit 0
